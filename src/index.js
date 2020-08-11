c32ff38de255d7dea8991cef7c31d63b64cafa41
Vannyou Tang <vannyoutang23@gmail.com>
2020-08-11 16:18:06 +0700

    asset management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+import DataXML from './data.xml';
+import DataCSV from './data.csv';
+import DataJSON from './data.json';
 
 function component() {
     const element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'Webpack'], ' ');
+    element.classList.add('hello');
+
+    const myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
+
+    console.log('Data XML', DataXML);
+    console.log('Data CSV', DataCSV);
+    console.log('Data JSON', DataJSON);
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,36 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(csv|tsv)$/,
+                use: ['csv-loader']
+            },
+            {
+                test: /\.xml$/,
+                use: ['xml-loader']
+            }
+        ]
     }
 };
