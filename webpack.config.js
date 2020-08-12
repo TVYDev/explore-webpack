b4c5f394702a33e1dd2581d56a4c27ca385b7897
Vannyou Tang <vannyoutang23@gmail.com>
2020-08-12 15:32:36 +0700

    code splitting using entry dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -22,8 +22,12 @@ module.exports = {
     ],
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
+        path: path.resolve(__dirname, 'dist')
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
     },
     module: {
         rules: [
