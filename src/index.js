cc11f1baaf99156fbcb9791fd6e4f2aaf1335559
Vannyou Tang <vannyoutang23@gmail.com>
2020-08-12 10:46:07 +0700

    Output management'

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import printMe from './print.js';
 import './style.css';
 import Icon from './icon.png';
 import DataXML from './data.xml';
@@ -7,13 +8,19 @@ import DataJSON from './data.json';
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
     element.innerHTML = _.join(['Hello', 'Webpack'], ' ');
     element.classList.add('hello');
 
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
     const myIcon = new Image();
     myIcon.src = Icon;
+
     element.appendChild(myIcon);
+    element.appendChild(btn);
 
     console.log('Data XML', DataXML);
     console.log('Data CSV', DataCSV);
