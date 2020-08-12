fc6795bcec981410658eebc47c9024c481e6a778
Vannyou Tang <vannyoutang23@gmail.com>
2020-08-12 16:03:39 +0700

    code splitting using dynamic import

--- src/index.js
+++ src/index.js
@@ -1,32 +1,13 @@
-import _ from 'lodash';
-import printMe from './print.js';
-import './style.css';
-import Icon from './icon.png';
-import DataXML from './data.xml';
-import DataCSV from './data.csv';
-import DataJSON from './data.json';
-
-function component() {
-    const element = document.createElement('div');
-    const btn = document.createElement('button');
-
-    element.innerHTML = _.join(['Hello', 'Webpack'], ' ');
-    element.classList.add('hello');
-
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-
-    const myIcon = new Image();
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
-    element.appendChild(btn);
-
-    console.log('Data XML', DataXML);
-    console.log('Data CSV', DataCSV);
-    console.log('Data JSON', DataJSON);
-
-    return element;
+function getComponent() {
+    return import(/* webpackChunkName: "lodash" */ 'lodash')
+        .then(({ default: _ }) => {
+            const element = document.createElement('div');
+            element.innerHTML = _.join(['Hello', 'Webpack'], ' ');
+            return element;
+        })
+        .catch((error) => 'An error occurred while loading the component');
 }
 
-document.body.appendChild(component());
+getComponent().then((component) => {
+    document.body.appendChild(component);
+});
