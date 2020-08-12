e1e780651122ee9066059e005bdbf21e1bc94ac0
Vannyou Tang <vannyoutang23@gmail.com>
2020-08-12 15:07:29 +0700

    development

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,27 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: {
         app: './src/index.js',
         print: './src/print.js'
     },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
     plugins: [
-        new CleanWebpackPlugin(),
+        new CleanWebpackPlugin({
+            cleanStaleWebpackAssets: false //Not removing index.html after incremental build triggered by webpack --watch
+        }),
         new HtmlWebpackPlugin({
             title: 'Output Management'
         })
     ],
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
     },
     module: {
         rules: [
